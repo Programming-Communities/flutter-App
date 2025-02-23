614a8711fa94a4ce65fd7fcc67683e48f5e005b2
Aamir Shahzadah <aamirshahzadah@gmail.com>
2025-02-23 08:13:02 +0500

    Shape Formation Letter A

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,129 @@
+import 'dart:math';
+import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: ParticleLetterA(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class ParticleLetterA extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  _ParticleLetterAState createState() => _ParticleLetterAState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class _ParticleLetterAState extends State<ParticleLetterA>
+    with SingleTickerProviderStateMixin {
+  late Ticker _ticker;
+  List<Particle> particles = [];
+  final int maxParticles = 200;
+  late Size screenSize;
+  double hue = 0;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  final List<Offset> shapePoints = [];
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  void initState() {
+    super.initState();
+    _ticker = createTicker((_) {
+      setState(() {
+        hue += 0.5;
+        for (var p in particles) {
+          p.update();
+        }
+      });
     });
   }
 
   @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    screenSize = MediaQuery.of(context).size;
+    _initializeParticles();
+    _ticker.start();
+  }
+
+  void _initializeParticles() {
+    shapePoints.clear();
+    shapePoints.addAll([
+      Offset(screenSize.width * 0.4, screenSize.height * 0.8),
+      Offset(screenSize.width * 0.5, screenSize.height * 0.2),
+      Offset(screenSize.width * 0.6, screenSize.height * 0.8),
+      Offset(screenSize.width * 0.45, screenSize.height * 0.5),
+      Offset(screenSize.width * 0.55, screenSize.height * 0.5)
+    ]);
+
+    particles.clear();
+    for (int i = 0; i < maxParticles; i++) {
+      particles.add(Particle(screenSize, shapePoints, hue));
+    }
+  }
+
+  @override
+  void dispose() {
+    _ticker.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      backgroundColor: Colors.black,
+      body: CustomPaint(
+        painter: ParticlePainter(particles),
+        child: Container(),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+class Particle {
+  late Offset position;
+  late Offset target;
+  late double velocity;
+  late double size;
+  double hue;
+  final Random random = Random();
+
+  Particle(Size screenSize, List<Offset> shapePoints, this.hue) {
+    position = Offset(
+      screenSize.width * random.nextDouble(),
+      screenSize.height * random.nextDouble(),
+    );
+    velocity = random.nextDouble() * 2 + 0.5;
+    size = random.nextDouble() * 3 + 2;
+    target = shapePoints[random.nextInt(shapePoints.length)];
+  }
+
+  void update() {
+    double dx = target.dx - position.dx;
+    double dy = target.dy - position.dy;
+    double angle = atan2(dy, dx);
+    position = Offset(
+      position.dx + velocity * cos(angle),
+      position.dy + velocity * sin(angle),
+    );
+  }
+}
+
+class ParticlePainter extends CustomPainter {
+  List<Particle> particles;
+
+  ParticlePainter(this.particles);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+
+    for (var p in particles) {
+      paint.color = HSLColor.fromAHSL(1, p.hue, 1, 0.5).toColor();
+      canvas.drawCircle(p.position, p.size, paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
 }
